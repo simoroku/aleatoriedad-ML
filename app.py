514e731e7a51efb20fbbb912791917c366417c6f
Sergio V <161333058+simoroku@users.noreply.github.com>
2025-03-23 01:53:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 
 # 📌 Cargar el modelo guardado
-@st.cache_data(allow_output_mutation=True)
+@st.cache_resource
 def cargar_modelo():
     modelo = load_model("modelo_baloto.h5")
     scaler = joblib.load("scaler_baloto.pkl")
